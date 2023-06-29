a72f6827be284424ed5403a5c7a678386e015b1e
BeautyJingMac <kidmac5321@gmail.com>
2023-06-29 17:37:21 +0800

    LeetCode 這次題目就有按照順序解答，但若題目超大的話就要花更多時間找到答案

--- ques00001.py
+++ ques00001.py
@@ -27,42 +27,79 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 # print(randomitem)
 
 ##############################################
+# import random
+# def findrandomitem(array):
+#     randomitemArray= random.sample(array,2)
+#     return randomitemArray
+#
+# array = [2,7,11,15]
+# answer = 9
+#
+# arrayNum = len(array) # 這個array當中有多少個element
+# listarray = list(range(arrayNum))
+# # ran = findrandomitem(listarray)
+# # print(ran)
+#
+# k=0
+# def caculateAnswer(array,listarray,k):
+#     if k==0:
+#         print(f"LeedCode Question:")
+#         print(f"initarray= {array}")
+#         print(f"listarray= {listarray}")
+#     k += 1
+#     print(f"=========it's {k} times=========")
+#
+#     randomitem = findrandomitem(listarray)
+#     print(randomitem)
+#     num1 = array[randomitem[0]]
+#     num2 = array[randomitem[1]]
+#     # print(num1,num2)
+#     if num1+num2 == answer:
+#         print(f"cuz num1 + num2 = answer = {answer}, stop method")
+#         print(f"answer= {randomitem}")
+#         print(f"num1= {num1}, radomitem= {randomitem[0]}")
+#         print(f"num2= {num2}, radomitem= {randomitem[1]}")
+#     else:
+#         caculateAnswer(array,listarray,k) #若沒有找到解答，那就重新random找答案
+#
+#
+# caculateAnswer(array,listarray,k)
+
+
+##############################################
+
 import random
-def findrandomitem(array):
-    randomitemArray= random.sample(array,2)
-    return randomitemArray
+import itertools
 
 array = [2,7,11,15]
 answer = 9
 
 arrayNum = len(array) # 這個array當中有多少個element
 listarray = list(range(arrayNum))
-# ran = findrandomitem(listarray)
-# print(ran)
+
+combinations = list(itertools.combinations(listarray,2))
+# print(combinations)
 
 k=0
-def caculateAnswer(array,listarray,k):
-    if k==0:
+def caculateAnswer(array,listarray,combinations,k):
+    if k ==0:
         print(f"LeedCode Question:")
         print(f"initarray= {array}")
         print(f"listarray= {listarray}")
-    k += 1
-    print(f"=========it's {k} times=========")
 
-    randomitem = findrandomitem(listarray)
-    print(randomitem)
-    num1 = array[randomitem[0]]
-    num2 = array[randomitem[1]]
-    # print(num1,num2)
-    if num1+num2 == answer:
-        print(f"cuz num1 + num2 = answer = {answer}, stop method")
-        print(f"answer= {randomitem}")
-        print(f"num1= {num1}, radomitem= {randomitem[0]}")
-        print(f"num2= {num2}, radomitem= {randomitem[1]}")
-    else:
-        caculateAnswer(array,listarray,k) #若沒有找到解答，那就重新random找答案
+    for combination in combinations:
+        k += 1
+        print(f"=========it's {k} times=========")
+        num1 = array[combination[0]]
+        num2 = array[combination[1]]
+        print(num1,num2)
+        if num1 + num2 == answer:
+            print(f"cuz num1 + num2 = answer = {answer}, stop method")
+            print(f"answer= {combination}")
+            print(f"num1= {num1}, sequence in array= {combination[0]}")
+            print(f"num2= {num2}, sequence in array= {combination[1]}")
+            break
+        else:
+            continue
 
-
-caculateAnswer(array,listarray,k)
-
-
+caculateAnswer(array,listarray,combinations,k)
